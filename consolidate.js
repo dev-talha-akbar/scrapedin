f70971e3c6645fbde5a7746a3a43894cbfa8af5a
Muhammad Talha Akbar <dev.talha.akbar@gmail.com>
2019-12-30 22:21:40 +0500

    Consolidating raw data script and ignoring all files

---
+++ consolidate.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const allProfiles = [];
+
+for (let i = 0; i <= 12; i++) {
+  console.log(`page${i}.json`);
+  const rawJSON = fs.readFileSync(`page${i}.json`);
+  const rawJSONObject = JSON.parse(rawJSON);
+
+  for (let j = 0; j < rawJSONObject.data.paging.count; j++) {
+    const profile = rawJSONObject.included[j];
+
+    if (profile && profile.publicIdentifier) {
+      allProfiles.push({
+        name: `${profile.firstName} ${profile.lastName}`,
+        occupation: profile.occupation,
+        username: profile.publicIdentifier,
+        avatar:
+          profile.picture &&
+          `${profile.picture.rootUrl}${profile.picture.artifacts[0].fileIdentifyingUrlPathSegment}`
+      });
+    }
+  }
+}
+
+console.log(`allProfiles Count: ${allProfiles.length}`);
+
+fs.writeFileSync("consolidated.json", JSON.stringify(allProfiles));
